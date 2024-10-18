e3afb8f25dfb1ad39ef49b75352374354ba76f98
Tanuj Kumar <tanujmaurya40@gmail.com>
2024-10-18 23:06:55 +0530

    Few Changes in index.js

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
